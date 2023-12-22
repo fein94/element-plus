e012e43ca80a1355bdde4aeec59c308ed2fe7e8b
fein94 <131580376+fein94@users.noreply.github.com>
2023-12-22 09:58:50 +0800

    Update basic.vue

--- docs/examples/tabs/basic.vue
+++ docs/examples/tabs/basic.vue
@@ -3,7 +3,6 @@
     <el-tab-pane label="User" name="first">User</el-tab-pane>
     <el-tab-pane label="Config" name="second">Config</el-tab-pane>
     <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
   </el-tabs>
 </template>
 <script lang="ts" setup>
